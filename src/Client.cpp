4542474d2ded02abe77a473ed260b813eddb4bae
idkhl <ines.dakhlaoui@gmail.com>
2025-05-20 18:43:29 +0200

    merge + rpl/err

--- src/Client.cpp
+++ src/Client.cpp
@@ -5,9 +5,9 @@ void Client::setAuthorization(int fd, const bool& allowed)
 {
     if (_allowed == false)
     {
-        	reply(fd, RPL_WELCOME, " :Welcome to the IRC server");
-			reply(fd, RPL_YOURHOST, " :Your host is localhost");
-			reply(fd, RPL_CREATED, " :This server was created today");
+        reply(fd, RPL_WELCOME, " :Welcome to the IRC server");
+        reply(fd, RPL_YOURHOST, " :Your host is localhost");
+        reply(fd, RPL_CREATED, " :This server was created today");
     }
     _allowed = allowed; 
 }
