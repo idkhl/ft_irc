51d05fa492752b64b6d0c49ac5a16c81f9ba1247
Yann Rio <yrio@f1r11s20.clusters.42paris.fr>
2025-05-21 15:49:47 +0200

    modif

--- src/Server.cpp
+++ src/Server.cpp
@@ -90,7 +90,6 @@ static std::vector<std::string> splitInput(std::string str)
 		if (str[i] == '\n')
 			str[i] = ' ';
 	}
-
 	std::vector<std::string> result;
 	size_t start = 0;
 	while (start < str.size())
