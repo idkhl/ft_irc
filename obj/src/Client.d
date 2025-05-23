1f016d9f8931e300f0f152d25acbae1dd570fa58
nojia1775 <nojia.adjemrose@gmail.com>
2025-05-23 15:25:35 +0200

    kick

--- obj/src/Client.d
+++
@@ -1,3 +0,0 @@
-obj/src/Client.o: src/Client.cpp src/../include/Client.hpp \
-  src/../include/Server.hpp src/../include/Colors.h \
-  src/../include/Channel.hpp
--- obj/src/Modes.d
+++
@@ -1,3 +0,0 @@
-obj/src/Modes.o: src/Modes.cpp src/../include/Server.hpp \
-  src/../include/Client.hpp src/../include/Channel.hpp \
-  src/../include/Colors.h
